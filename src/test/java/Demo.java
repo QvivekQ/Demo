7e5b1707d5ea63de2f1feae2389890fc296fa4d0
qolsys <vivek.tripathi@qolsys.com>
2018-05-07 18:57:24 +0530

    Test

--- src/test/java/Demo.java
+++ src/test/java/Demo.java
@@ -1,7 +1,19 @@
-public class Demo {
-    public static void main(String[] args) {
-        System.out.println("hello");
-        System.out.println("hello");
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
 
-    }
+@Test()
+public class Demo {
+
+
+        @DataProvider
+        public Object[][] data() {
+            return new String[][] {new String[] {"data1"}, new String[] {"data2"}};
+        }
+
+        @Test(dataProvider = "data")
+        public void test(String d) {
+            Assert.assertEquals("First Line>\nSecond Line", "Third Line\nFourth Line");
+        }
+
 }
